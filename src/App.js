406b8d4ceb4f3f636258cf639aa7c55eb9bf83bb
offiahchiaz <offiahchiaz@gmail.com>
2022-05-12 04:57:10 +0100

    Refactored App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import jumboData from './fixtures/jumbo.json'
+import Jumbotron from "./components/jumbotron";
 
 
 export default function App() {
   return (
-    <p>Hello</p>
+    <div>
+      <Jumbotron.Container>
+        {jumboData.map((item) => (
+          <Jumbotron key={item.id} direction={item.direction}>
+            <p>Hello</p>
+          </Jumbotron>
+        ))}
+      </Jumbotron.Container>
+    </div>
   );
 }
 
