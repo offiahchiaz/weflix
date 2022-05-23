f6e00b38416b52d852e4fc12d1b3dac47fa3493f
offiahchiaz <offiahchiaz@gmail.com>
2022-05-23 23:16:19 +0100

    Updated App component with new routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import { FaqsContainer } from "./containers/faqs";
-import { FooterContainer } from "./containers/footer";
-import { JumbotronContainer } from "./containers/jumbotron";
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import * as ROUTES from './constants/routes'
+import { Home, Browse, Signin, Signup } from './pages'
+
 
 
 export default function App() {
   return ( 
-    <>
-      <JumbotronContainer/>
-      <FaqsContainer />
-      <FooterContainer />
-    </> 
+    <BrowserRouter>
+      <Switch>
+        <Route path={ROUTES.BROWSE}>
+          <Browse />
+        </Route>
+        <Route path={ROUTES.SIGN_IN}>
+          <Signin />
+        </Route>
+        <Route path={ROUTES.SIGN_UP}>
+          <Signup />
+        </Route>
+        <Route exact path={ROUTES.HOME}>
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter> 
   );
 }
 
