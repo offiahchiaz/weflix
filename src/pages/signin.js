4643c2d82bb9d57ce5a22066dc1c8a7d0c810115
offiahchiaz <offiahchiaz@gmail.com>
2022-05-27 02:55:10 +0100

    Added Signin component

---
+++ src/pages/signin.js
@@ -0,0 +1,11 @@
+import { FooterContainer } from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
+
+export default function Signin() {
+  return (
+    <d>
+      <HeaderContainer>This is signin</HeaderContainer>
+      <FooterContainer />
+    </d>
+  )
+}
